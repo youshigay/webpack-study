5352720f285d01ded9e5f3e9d5dc52db31fed56f
游志刚 <youzhigang228@C02SDA9PFVH5.local>
2018-07-11 13:51:08 +0800

    使用webpack-dev-server等

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 import _ from 'lodash';
+import printMe from './print';
 import './style.css';
-import bgImg from './bg.png';
-import Data from './data.xml';
 
 function component() {
-  var element = document.createElement('div');
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  const bgImage = new Image();
-  bgImage.src = bgImg;
+  btn.innerHTML = 'click me and check the console';
+  btn.onclick = printMe;
 
-  element.appendChild(bgImage);
-
-  console.log(Data);
+  element.appendChild(btn);
+ 
   return element;
 }
 
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if (module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    document.body.removeChild(element);
+    element = component(); // 重新渲染页面后，component 更新 click 事件处理
+    document.body.appendChild(element);
+  })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'production',
+  entry: {
+    app: './src/index.js',
+    // print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }, {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader',
-        ]
-      }, {
-        test: /\.(csv|tsv)$/,
-        use: [
-          'csv-loader',
-        ]
-      }, {
-        test: /\.xml$/,
-        use: [
-          'xml-loader'
-        ]
+        use: ['style-loader', 'css-loader']
       }
     ]
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'output management',
+    }),
+    new webpack.NamedModulesPlugin,
+    new webpack.HotModuleReplacementPlugin,
+  ],
 };
