78f5661fdfeb4caf9364898e208aee515c1d2913
游志刚 <youzhigang228@C02SDA9PFVH5.local>
2018-07-11 16:01:21 +0800

    加入bundle-analyzer

--- webpack.prd.js
+++ webpack.prd.js
@@ -2,10 +2,14 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   devtool: 'source-map',
   plugins: [
+    new BundleAnalyzerPlugin({
+      generateStatsFile: true,
+    }),
     new UglifyJSPlugin({
       sourceMap: true,
     }),
